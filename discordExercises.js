4e7c28ed49a77bc7bc4bd7a02afefe291676ed7c
markisgod92 <markisgod92@gmail.com>
2024-05-24 21:02:53 +0200

    first commit

---
+++ discordExercises.js
@@ -0,0 +1,92 @@
+// Esercizio 1: Trova il numero più grande
+// Descrizione: Scrivi una funzione maxNumber che prende in input un ARRAY di numeri e restituisce il 
+// numero più grande nell'array.
+
+// es: maxNumber([1, 2, 3, 4, 5]); // -> l'output dovrebbe essere 5
+
+function maxNumber(array) {
+    let highest = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > highest) {
+            highest = array[i];
+        };
+    }
+    return highest;
+};
+
+console.log(maxNumber([1, 2, -15, 40, -75, 120]))
+
+
+
+/*
+Esercizio 2: Palindromo
+Descrizione: Scrivi una funzione isPalindrome che prende in input una stringa e restituisce true se la stringa è un palindromo (si legge uguale da entrambi i lati) e false altrimenti.
+*/
+
+/*
+isPalindrome("anna"); // true
+isPalindrome("hello"); // false
+*/
+
+// suggerimento: metodi delle stringhe e degli array ti torneranno utili come ad esempio il metodo 
+// reverse() (googlalo su MDN)
+
+function isPalindrome(stringa) {
+    let stringReversed = ``;
+    
+    for (i = stringa.length - 1; i >= 0; i--) {
+        stringReversed += stringa[i];
+    };
+
+    return stringa === stringReversed;
+
+}
+
+console.log(isPalindrome("anna"))
+
+
+
+/*
+Esercizio 4: Somma di numeri pari
+Descrizione: Scrivi una funzione sumEvenNumbers che prende in input un array di numeri e restituisce la somma di tutti i numeri pari nell'array.
+
+sumEvenNumbers([10, 20, 30, 40]); // 100
+*/
+
+function sumEvenNumbers() {
+
+}
+
+/*
+Esercizio 5: Rimuovi duplicati
+Descrizione: Scrivi una funzione removeDuplicates che prende in input un array di numeri e restituisce un nuovo array senza duplicati.
+
+suggerimento... includes() potrebbe essere il metodo che fa per te...o forse no?  
+
+es: removeDuplicates([1, 2, 2, 3, 4, 4, 5]); // [1, 2, 3, 4, 5]
+*/
+
+function removeDuplicates() {
+  
+}
+
+/*
+Esercizio 7: Contare vocali
+Descrizione: Scrivi una funzione countVowels che prende in input una stringa e restituisce il numero di vocali presenti nella stringa.
+
+countVowels("javascript"); // 3
+*/
+
+function countVowels(parola) {
+    parola = parola.toLowerCase()
+    const vowels = "aeiou";
+    let contatore = 0;
+    for (i = 0; i < parola.length; i++) {
+        if (vowels.includes(parola[i])) {
+            contatore++;
+        };
+    };
+    return contatore;
+};
+
+console.log(countVowels("MARCO"))
